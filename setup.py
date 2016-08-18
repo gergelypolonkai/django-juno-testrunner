e7e6a28574360b1abba2918d89fe5de3cfacd41c
Gergely Polonkai <gergely.polonkai@ericsson.com>
2016-08-18 16:30:09 +0200

    Add tox test to setup.py
    This way tox will run whenever `setup.py test` runs.

--- setup.py
+++ setup.py
@@ -1,10 +1,38 @@
 import os
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# Got this from the tox web site
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', 'Arguments to pass to tox')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+
+        args = self.tox_args
+
+        if args:
+            args = shlex.split(self.tox_args)
+
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
 
 setup(
     name='django-juno-testrunner',
@@ -18,6 +46,8 @@ setup(
     packages=['junorunner'],
     install_requires=['colorama'],
     extras_require={'junorunner': ['colorama', ]},
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
     classifiers=[
             'Environment :: Web Environment',
             'Framework :: Django',
