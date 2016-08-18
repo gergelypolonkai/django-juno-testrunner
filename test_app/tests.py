ea97b67eeee303d416f82cd718267e40d5e6df2f
Gergely Polonkai <gergely.polonkai@ericsson.com>
2016-08-18 11:26:35 +0200

    Try to achieve better coverage

--- test_app/tests.py
+++ test_app/tests.py
@@ -1,4 +1,13 @@
+import time
+
 from django.test import TestCase, TransactionTestCase
+
+try:
+    # Django 1.6
+    from django.utils import unittest
+except ImportError:
+    # Django 1.7+ because bundled unittest is going away
+    import unittest
 
 
 class JunorunnerTestCase(TestCase):
@@ -18,3 +27,21 @@ class JunorunnerTransactionTestCase(TransactionTestCase):
 
     def test_can_run_transaction_bound_tests(self):
         pass
+
+    def test_erring_test(self):
+        self.this_method_clearly_doesnt_exist()
+
+    def test_failing_test(self):
+        self.assertEqual(1, 2)
+
+    @unittest.skip("Let's skip this")
+    def test_skipped(self):
+        pass
+
+    @unittest.expectedFailure
+    def test_expected_failure(self):
+        self.assertEqual(1, 2, "Well, this was expected")
+
+    @unittest.expectedFailure
+    def test_unexpected_success(self):
+        self.assertEquals(1, 1, "You don't say...")
